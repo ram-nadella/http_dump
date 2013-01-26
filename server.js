2af54149413494d0085a9a4e09fe5f248081b20c
Ram <ram@cloudflare.com>
2013-01-25 22:27:35 -0800

    The server now dumps some interesting stuff

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var http = require('http');
 var port = process.env.PORT || 5000;
 http.createServer(function(req, res) {
-    res.end('Hello World\n');
+    var request_body = "";
+    req.on("data", function(chunk) {
+        request_body += chunk;
+    });
+
+    var response_data = {};
+    req.on("end", function() {
+        response_data = {
+            "method": req.method,
+            "version": req.httpVersion,
+            "url": req.url,
+            "headers": req.headers,
+            "body": request_body,
+            "ip": ("X-Forwarded-For" in req.headers)? req.headers["X-Forwarded-For"] : req.connection.remoteAddress
+        };
+
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Server", "HTTP Dumper");
+        res.end(JSON.stringify(response_data) + "\n");
+    });
 }).listen(port, function() {
     console.log("Server listening on port " + port);
 });
